1edd15fe430ad26ee339d9789e27519ccf41ea14
YoshinoriIwaki <tanlab@hotmail.co.jp>
2018-02-12 18:16:06 +0900

    neural network demo

--- 3_NeuralNetwork.py
+++ 3_NeuralNetwork.py
@@ -92,16 +92,16 @@ B3 = np.array([0.1, 0.2])
 A3 = np.dot(Z2, W3) + B3
 Y = identity_function(A3)
 
-def init_network():
-    network = {}
-    network['W1'] = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
-    network['b1'] = np.array([0.1, 0.2, 0.3])
-    network['W2'] = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
-    network['b2'] = np.array([0.1, 0.2])
-    network['W3'] = np.array([[0.1, 0.3], [0.2, 0.4]])
-    network['b3'] = np.array([0.1, 0.2])
+# def init_network():
+#     network = {}
+#     network['W1'] = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
+#     network['b1'] = np.array([0.1, 0.2, 0.3])
+#     network['W2'] = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
+#     network['b2'] = np.array([0.1, 0.2])
+#     network['W3'] = np.array([[0.1, 0.3], [0.2, 0.4]])
+#     network['b3'] = np.array([0.1, 0.2])
 
-    return network
+#     return network
 
 def forward(network, x):
     W1, W2, W3 = network['W1'], network['W2'], network['W3']
@@ -116,10 +116,10 @@ def forward(network, x):
 
     return y
 
-network = init_network()
-x = np.array([1.0, 0.5])
-y = forward(network, x)
-print(y)
+# network = init_network()
+# x = np.array([1.0, 0.5])
+# y = forward(network, x)
+# print(y)
 
 def softmax(a):
     c = np.exp(a)
@@ -135,14 +135,117 @@ y = softmax(a)
 print("y ", y)
 print("np.sum(y) ", np.sum(y))
 
+# import sys, os
+# sys.path.append(os.pardir)
+# from dataset.mnist import load_mnist
+
+# (x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False)
+
+# print(x_train.shape)
+# print(t_train.shape)
+# print(x_test.shape)
+# print(t_test.shape)
+
 import sys, os
+import pickle
 sys.path.append(os.pardir)
+import numpy as np
 from dataset.mnist import load_mnist
+from PIL import Image
 
-(x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False)
+def img_show(img):
+    pil_img = Image.fromarray(np.uint8(img))
+    pil_img.show()
 
-print(x_train.shape)
-print(t_train.shape)
-print(x_test.shape)
-print(t_test.shape)
+(x_train, t_train), (x_test, t_test) = load_mnist(flatten = True, normalize = False)
+img = x_train[0]
+label = t_train[0]
+print(label)
 
+print(img.shape)
+img = img.reshape(28, 28)
+print(img.shape)
+
+img_show(img)
+
+def get_data():
+    (x_train, t_train), (x_test, t_test) = load_mnist(normalize = True, flatten = True, one_hot_label = False)
+    return x_test, t_test
+
+def init_network():
+    with open("sample_weight.pkl", 'rb') as f:
+        network = pickle.load(f)
+
+    return network
+
+def predict(network, x):
+    W1, W2, W3 = network['W1'], network['W2'], network['W3']
+    b1, b2, b3 = network['b1'], network['b2'], network['b3']
+
+    a1 = np.dot(x, W1) + b1
+    z1 = sigmoid(a1)
+    a2 = np.dot(z1, W2) + b2
+    z2 = sigmoid(a2)
+    a3 = np.dot(z2, W3) + b3
+    y = softmax(a3)
+
+    return y
+
+x, t = get_data()
+network = init_network()
+
+accuracy_cnt = 0
+for i in range(len(x)):
+    y = predict(network, x[i])
+    p = np.argmax(y)
+
+    if p == t[i]:
+        accuracy_cnt += 1
+
+print("Accuracy:" + str(float(accuracy_cnt) / len(x)))
+
+x, _ = get_data()
+network = init_network()
+W1, W2, W3 = network['W1'], network['W2'], network['W3']
+
+print(x.shape)
+print(x[0].shape)
+print(W1.shape)
+print(W2.shape)
+print(W3.shape)
+
+x, t = get_data()
+network = init_network()
+
+batch_size = 100
+accuracy_cnt = 0
+
+for i in range(0, len(x), batch_size):
+    x_batch = x[i:i+batch_size]
+    y_batch = predict(network, x_batch)
+    p = np.argmax(y_batch, axis=1)
+    accuracy_cnt += np.sum(p == t[i:i+batch_size])
+
+print("Accuracy:" + str(float(accuracy_cnt) / len(x)))
+
+y = [0.1, 0.05, 0.6, 0.0, 0.05, 0.1, 0.0, 0.1, 0.0, 0.0]
+t = [0, 0, 1, 0, 0, 0, 0, 0, 0, 0]
+
+def mean_squared_error(y, t):
+    return 0.5 * np.sum((y-t)**2)
+
+print(mean_squared_error(np.array(y), np.array(t)))
+
+y = [0.1, 0.05, 0.1, 0.0, 0.05, 0.1, 0.0, 0.6, 0.0, 0.0]
+print(mean_squared_error(np.array(y), np.array(t)))
+
+def cross_entropy_error(y, t):
+    delta = 1e-7
+    return -np.sum(t * np.log(y + delta))
+
+t = [0, 0, 1, 0, 0, 0, 0, 0, 0, 0]
+y = [0.1, 0.05, 0.6, 0.0, 0.05, 0.1, 0.0, 0.1, 0.0, 0.0]
+print(cross_entropy_error(np.array(y), np.array(t)))
+
+y = [0.1, 0.05, 0.1, 0.0, 0.05, 0.1, 0.0, 0.6, 0.0, 0.0]
+print(cross_entropy_error(np.array(y), np.array(t)))
